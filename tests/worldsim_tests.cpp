3ad24b37ddf95f53cc2c5a780e2c5e82f642e31c
Gorills <57192423+Gorills@users.noreply.github.com>
2026-09-08 13:50:43 +0700

    test: reject pre-flora snapshot epoch

--- tests/worldsim_tests.cpp
+++ tests/worldsim_tests.cpp
@@ -299,7 +299,8 @@ void test_determinism_and_snapshot() {
     for (std::uint8_t legacy_version:{
         std::uint8_t{2},std::uint8_t{3},std::uint8_t{4},std::uint8_t{5},
         std::uint8_t{6},std::uint8_t{7},std::uint8_t{8},std::uint8_t{9},
-        std::uint8_t{10},std::uint8_t{11},std::uint8_t{12}
+        std::uint8_t{10},std::uint8_t{11},std::uint8_t{12},
+        std::uint8_t{13}
     }) {
         auto legacy_snapshot=snap;
         legacy_snapshot[8]=static_cast<std::byte>(legacy_version);
